4a2df9282c2db6e208f78903b306059434937f67
Sophie Herold <sophie@hemio.de>
2023-04-21 19:15:29 +0200

    0.6.1

--- fusermount-wrapper.sh
+++ fusermount-wrapper.sh
@@ -10,4 +10,10 @@ if [ -e /proc/self/fd/3 ] && [ 3 != "$_FUSE_COMMFD" ]; then
     FD_ARGS="$FD_ARGS --forward-fd=3"
 fi
 
-exec flatpak-spawn --host --forward-fd=1 --forward-fd=2 $FD_ARGS fusermount "$@"
+# If the fusermount3 binary doesn't exist we try fusermount
+# command -v will return 0 if the command exists and 127 otherwise
+if flatpak-spawn --host sh -c "command -v fusermount3" > /dev/null; then
+    exec flatpak-spawn --host --forward-fd=1 --forward-fd=2 $FD_ARGS fusermount3 "$@"
+else
+    exec flatpak-spawn --host --forward-fd=1 --forward-fd=2 $FD_ARGS fusermount "$@"
+fi
